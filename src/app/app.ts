2bc66230eaf323a7f5806063368b61fa6251c326
Yinghan Wang <wangx6@gmail.com>
2025-11-15 20:53:59 +0000

    start

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,22 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('todo');
+  count = signal(0);
+
+  todos = signal([
+    { id: 1, title: 'buy milk', completed: false },
+    { id: 2, title: 'todo 2', completed: true },
+    { id: 3, title: 'todo 3', completed: false }
+  ]);
+
+
+  increment() {
+    this.count.update(count => count + 1);
+    // this.count.set(this.count() + 1);
+  }
+
+  reset() {
+    this.todos.update(todos => todos.map(t => ({ ...t, completed: false })));
+  }
+
 }
